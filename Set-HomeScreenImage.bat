ca03d2b069c616ee9a49156071605142903ddc7c
MJDawson <66313685+MJDaws0n@users.noreply.github.com>
2026-05-11 14:46:41 +0100

    Update Set-HomeScreenImage.bat

--- Set-HomeScreenImage.bat
+++ Set-HomeScreenImage.bat
@@ -3,6 +3,6 @@ setlocal
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Set-HomeScreenImage.ps1" %*
 set exit_code=%ERRORLEVEL%
 if not "%exit_code%"=="0" goto :end
-if exist "%LOCALAPPDATA%\Microsoft\Windows\Themes\67Theme.theme" start "" "%LOCALAPPDATA%\Microsoft\Windows\Themes\67Theme.theme"
+if exist "%LOCALAPPDATA%\Microsoft\Windows\Themes\67Custom.theme" start "" "%LOCALAPPDATA%\Microsoft\Windows\Themes\67Custom.theme"
 :end
 endlocal & exit /b %exit_code%
